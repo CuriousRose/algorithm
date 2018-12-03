58ee09fa54e5344d34438c643902cfa5f5ebca95
caohejing <caohejing@weshare.com.cn>
2018-12-03 11:02:06 +0800

    hdu 1040-1044

---
+++ src/hdu/hdu_1044/Main.java
@@ -0,0 +1,16 @@
+package hdu.hdu_1044;
+
+import java.util.Scanner;
+
+public class Main {
+
+    static Scanner scanner = new Scanner(System.in);
+
+    static int w, h, l, m;
+    static int[] mv;
+    static char[][] map;
+
+    public static void main(String[] args) {
+
+    }
+}
